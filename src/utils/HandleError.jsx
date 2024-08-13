143df72c33deb49acd6ef51fa08e10ed6a46f674
栖鸟闲榆 <59347549+coxine@users.noreply.github.com>
2024-08-13 17:24:00 +0800

    fix: 429error

--- src/utils/HandleError.jsx
+++ src/utils/HandleError.jsx
@@ -3,6 +3,11 @@ export const handle500Error = () => {
   throw new Error("服务器错误");
 };
 
+export const handle400Error = () => {
+  alert("请求数据无效");
+  throw new Error("请求数据无效");
+};
+
 export const handle401Error = () => {
   alert("未授权");
   localStorage.removeItem("token");
@@ -15,9 +20,9 @@ export const handle404Error = () => {
   throw new Error("用户不存在");
 };
 
-export const handle400Error = () => {
-  alert("请求数据无效");
-  throw new Error("请求数据无效");
+export const handle429Error = () => {
+  alert("请求过多");
+  throw new Error("请求过多");
 };
 
 export default function handleError(error) {
@@ -27,6 +32,8 @@ export default function handleError(error) {
     handle404Error();
   } else if (error.response && error.response.status === 400) {
     handle400Error();
+  } else if (error.response && error.response.status === 429) {
+    handle429Error();
   } else {
     handle500Error();
   }
